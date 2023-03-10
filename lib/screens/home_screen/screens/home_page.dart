87ca14072f51827e92898590bdaaf7254bda2c01
Meet Patel <patelmeet341993@gmail.com>
2023-03-10 19:04:01 +0530

    done

--- lib/screens/home_screen/screens/home_page.dart
+++ lib/screens/home_screen/screens/home_page.dart
@@ -19,40 +19,13 @@ class _HomePageState extends State<HomePage> {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   List<Article> data = [];
 
-  getTopics() {
-    firestore.collection("admin").doc("data").get().then((value) {
-      Map<dynamic, dynamic> data = value.data() as Map;
-      topics = data["topics"];
-      selectedItem = 0;
-      getArticles(topics[0]);
-      setState(() {});
-    });
-  }
 
-  void getArticles(String topic) {
-    data.clear();
-    setState(() {
 
-    });
-    firestore.collection(topic).get().then((value) {
-
-     List<QueryDocumentSnapshot> documentSnapshot= value.docs;
-     for(int i=0;i<documentSnapshot.length;i++)
-       {
-         Map<dynamic, dynamic> map = documentSnapshot[i].data() as Map;
-         data.add(Article.fromMap(map, documentSnapshot[i].id));
-         print("article : $map");
-       }
-      setState(() {
-
-      });
-    });
-  }
 
   @override
   void initState() {
     super.initState();
-    getTopics();
+
   }
 
   @override
@@ -61,68 +34,26 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           MyAppBar(
-            title: "Fire Safety",
+            title: "Table Controller App",
             color: Colors.white,
             backbtnVisible: false,
           ),
           SizedBox(
-            height: 10,
+            height: 100,
           ),
           Container(
-            height: 40,
-            child: ListView.builder(
-                itemCount: topics.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (ctx, index) {
-                  return InkWell(
-                    onTap: () {
-                      selectedItem = index;
-                      getArticles(topics[selectedItem]);
-                      setState(() {});
-                    },
-                    child: Container(
-                      height: 34,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color:
-                            selectedItem != index ? Colors.blue : Colors.indigo,
-                      ),
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                      margin: EdgeInsets.only(left: 10),
-                      child: Center(
-                          child: Text(
-                        topics[index],
-                        style: TextStyle(color: Colors.white),
-                      )),
-                    ),
-                  );
-                }),
+            color: Colors.blueGrey,
+            padding: EdgeInsets.all(50),
+            child: Icon(Icons.flip_camera_android,size: 50,),
           ),
           SizedBox(
-            height: 10,
+            height: 50,
           ),
-          Expanded(child: PageView.builder(
-                itemCount: data.length,
-              scrollDirection: Axis.vertical,
-              itemBuilder: (ctx, index) {
-
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CachedNetworkImage(imageUrl: data[index].img,fit: BoxFit.fitWidth,),
-                Padding(
-                  padding:  EdgeInsets.only(left: 15,top: 10,right: 15,bottom: 10),
-                  child: Text(data[index].title,style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold),),
-                ),
-                Padding(
-                  padding:  EdgeInsets.only(left: 15,top: 10,right: 15,bottom: 10),
-                  child: Text(data[index].des,style: TextStyle(color: Colors.black,fontSize: 13),),
-                )
-              ],
-            );
-
-          }))
+          Container(
+            color: Colors.blueGrey,
+            padding: EdgeInsets.all(50),
+            child: Icon(Icons.light_mode,size: 50,),
+          ),
         ],
       ),
     );
--- lib/screens/home_screen/screens/main_page.dart
+++ lib/screens/home_screen/screens/main_page.dart
@@ -114,9 +114,9 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
   }
 
   Widget getHome() {
-   // postListWidget ??=  HomePage();
+   postListWidget ??=  HomePage();
 
-    postListWidget ??=  HomePageAdmin();
+    //postListWidget ??=  HomePageAdmin();
   //  postListWidget ??=  AddFile();
     return postListWidget!;
   }
