7c433576cf36f1a4cb4798b6a4531501ab80220e
Meet Patel <patelmeet341993@gmail.com>
2023-03-12 12:50:31 +0530

    done

--- lib/screens/home_screen/screens/home_page.dart
+++ lib/screens/home_screen/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:inshorts/models/Article.dart';
 
@@ -19,12 +20,54 @@ class _HomePageState extends State<HomePage> {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   List<Article> data = [];
 
+  bool isFan=false;
+  bool isLight=false;
 
+  final  database=FirebaseDatabase.instance;
+  void getdata(){
+
+    print("calling get daya");
+
+    database.ref("fan").get().then((value) {
+     dynamic number= value.value.toString();
+     print("get data :  $number");
+
+     if(number=="1")
+       {
+         isFan=true;
+       }
+     else
+       {
+         isFan=false;
+       }
+     setState(() {
+
+     });
+    });
+
+    database.ref("light").get().then((value) {
+      dynamic number= value.value.toString();
+      print("get data :  $number");
+      if(number=="1")
+      {
+        isLight=true;
+      }
+      else
+      {
+        isLight=false;
+      }
+      setState(() {
+
+      });
+    });
+
+  }
 
 
   @override
   void initState() {
     super.initState();
+    getdata();
 
   }
 
@@ -41,21 +84,63 @@ class _HomePageState extends State<HomePage> {
           SizedBox(
             height: 100,
           ),
-          Container(
-            color: Colors.blueGrey,
-            padding: EdgeInsets.all(50),
-            child: Icon(Icons.flip_camera_android,size: 50,),
+          InkWell(
+            onTap: (){
+        onOffFan();
+            },
+            child: Container(
+              color: isFan?Colors.green:Colors.blueGrey,
+              padding: EdgeInsets.all(50),
+              child: Icon(Icons.flip_camera_android,size: 50,),
+            ),
           ),
           SizedBox(
             height: 50,
           ),
-          Container(
-            color: Colors.blueGrey,
-            padding: EdgeInsets.all(50),
-            child: Icon(Icons.light_mode,size: 50,),
+          InkWell(
+            onTap: (){
+      onOffLight();
+            },
+            child: Container(
+              color: isLight?Colors.green:Colors.blueGrey,
+              padding: EdgeInsets.all(50),
+              child: Icon(Icons.light_mode,size: 50,),
+            ),
           ),
         ],
       ),
     );
   }
+
+
+  void onOffLight(){
+    if(isLight)
+      {
+        database.ref("light").set(0);
+      }
+    else
+      {
+        database.ref("light").set(1);
+      }
+    isLight=!isLight;
+    setState(() {
+
+    });
+  }
+
+  void onOffFan(){
+    if(isFan)
+    {
+      database.ref("fan").set(0);
+    }
+    else
+    {
+      database.ref("fan").set(1);
+    }
+    isFan=!isFan;
+    setState(() {
+
+    });
+  }
+
 }
